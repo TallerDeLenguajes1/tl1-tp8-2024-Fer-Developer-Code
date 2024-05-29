f5a26df3fc49f1027cf88cfdf356f00db4a9eee1
Fernando <ibanez.fernando.2018@gmail.com>
2024-05-29 20:34:24 -0300

    Terminado punto 1? Probando funcionalidad

--- Program.cs
+++ Program.cs
@@ -25,27 +25,37 @@ do
             linea = Console.ReadLine();
             if (int.TryParse(linea, out idTareaBuscada))
             {
-                foreach (Tarea tareaBuscada in ListaTareasPendientes)
+                hayoTarea = buscarPorId(idTareaBuscada, hayoTarea, ListaTareasPendientes, ListaTareasRealizadas);
+                if (!hayoTarea)
                 {
-                    if (idTareaBuscada == tareaBuscada.TareaID)
+                    Console.WriteLine("No se encontro ninguna tarea con ese ID\nDesea buscarlas por descripcion?\n1 para si 2 para no");
+                    linea = Console.ReadLine();
+                    if (int.TryParse(linea, out opcion))
                     {
-                        hayoTarea = true;
-                        var auxTarea = tareaBuscada;
-                        auxTarea.EstadoActual = EstadoTarea.Realizada;
-                        ListaTareasRealizadas.Add(auxTarea);
-                        ListaTareasPendientes.Remove(tareaBuscada);
-                        Console.WriteLine("Tarea movida con exito");
-                        break;
+                        if (opcion == 1)
+                        {
+                            linea = Console.ReadLine();
+                            hayoTarea = mostarPorDescripcion(linea, hayoTarea, ListaTareasPendientes);
+                            if (!hayoTarea)
+                            {
+                                Console.WriteLine("No se encontro ninguna tarea con esa descripcion");
+                            }
+                        }
                     }
                 }
-                if(hayoTarea == false){
-                    Console.WriteLine("No se encontro ninguna tarea con ese ID\nDesea buscarlas por descripcion?");
-                }
+            }
+            else {
+                Console.WriteLine("Inserte un numero valido para operar");
             }
         }
-        else {
-            opcion = 2;
+        else if (opcion == 2)
+        {
             Console.WriteLine("Gracias por usar el gestor de tareas");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Inserte un numero valido para operar");
         }
     }
     else
@@ -85,4 +95,37 @@ static void asignarTareas(Random random, int cantTareas, List<Tarea> ListaTareas
         Tarea tareaPendiente = new Tarea(i + 1, $"Tarea numero: {i + 1}", tiempoDuracion, EstadoTarea.Pendiente);
         ListaTareasPendientes.Add(tareaPendiente);
     }
+}
+
+static bool buscarPorId(int idTareaBuscada, bool hayoTarea, List<Tarea> ListaTareasPendientes, List<Tarea> ListaTareasRealizadas)
+{
+    foreach (Tarea tareaBuscada in ListaTareasPendientes)
+    {
+        if (idTareaBuscada == tareaBuscada.TareaID)
+        {
+            hayoTarea = true;
+            var auxTarea = tareaBuscada;
+            auxTarea.EstadoActual = EstadoTarea.Realizada;
+            ListaTareasRealizadas.Add(auxTarea);
+            ListaTareasPendientes.Remove(tareaBuscada);
+            Console.WriteLine("Tarea movida con exito");
+            break;
+        }
+    }
+
+    return hayoTarea;
+}
+
+static bool mostarPorDescripcion(string? linea, bool hayoTarea, List<Tarea> ListaTareasPendientes)
+{
+    foreach (Tarea tareaBuscada in ListaTareasPendientes)
+    {
+        if (linea != null && tareaBuscada.Descripcion.Contains(linea))
+        {
+            hayoTarea = true;
+            Console.WriteLine($"\tID: {tareaBuscada.TareaID}, Descripcion: {tareaBuscada.Descripcion}, Duracion: {tareaBuscada.Duracion}, Estado: {tareaBuscada.EstadoActual}.");
+        }
+    }
+
+    return hayoTarea;
 }
